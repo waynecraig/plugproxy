f20427d8256696d79c136dfcc6626810d8db273a
Wayne Craig <waynecraig2008@gmail.com>
2016-03-01 17:21:38 +0800

    Object.assign polyfill

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var config = require('./config.json');
 var proxy = require('./modules/proxy');
 var ui = require('./modules/ui');
 
+// Object.assign polyfill
+Object.assign = require('object-assign');
+
 proxy.listen(config.proxyPort, function() {
     console.log('proxy server is listening port', config.proxyPort);
 });
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
+Object.assign = require('object-assign');
 
 import {
     default as React,
