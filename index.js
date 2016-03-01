147bc82ae245f896e231e0d63e386c9ee6d84f42
craigwu(吴泽鑫) <craigwu@tencent.com>
2016-03-01 17:02:22 +0800

    update

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var config = require('./config.json');
 var proxy = require('./modules/proxy');
 var ui = require('./modules/ui');
 
+var objectAssign = require('object-assign');
+if (!Object.assign) {
+    Object.assign = objectAssign;
+}
+
 proxy.listen(config.proxyPort, function() {
     console.log('proxy server is listening port', config.proxyPort);
 });
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
+Object.assign = require('object-assign');
+
 import {
     default as React,
     Component,
